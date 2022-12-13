7e4c66e1ba11a56c72dfe141613ef99176f89844
Kelvin ELenwo <kelvin@axumtechnologies.com>
2022-12-13 09:13:50 +0100

    Switched database type to SQLite3 from MySQL

--- school/settings.py
+++ school/settings.py
@@ -75,18 +75,22 @@ WSGI_APPLICATION = 'school.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hostel',
-        'USER': 'root',
-        'PASSWORD': '',
-        # 'HOST': '127.0.0.1',
-        'PORT': '3306',
-        'OPTIONS': {
-            'read_default_file': '/etc/mysql/my.cnf',
-        },
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     # 'ENGINE': 'django.db.backends.mysql',
+    #     # 'NAME': 'hostel',
+    #     # 'USER': 'root',
+    #     # 'PASSWORD': '',
+    #     # 'HOST': '127.0.0.1',
+    #     # 'PORT': '3306',
+    #     'OPTIONS': {
+    #         # 'read_default_file': '/etc/mysql/my.cnf',
+    #     },
+    #     # 'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+        'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
